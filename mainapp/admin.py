30a68caf2bc233e80b874d2e5453edd5c1c016eb
Pavel <prohorenkopavel74@gmail.com>
2021-03-21 16:58:43 +0500

    Initial commit

---
+++ mainapp/admin.py
@@ -0,0 +1,44 @@
+from PIL import Image
+
+from django.forms import ModelForm, ValidationError
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from .models import *
+
+
+class ProductAdminForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = mark_safe(
+            """<span style = "color : red; font-size : 14px;">Изображение должно быть больше чем {}x{}px, но меньше чем {}x{}px!</span>""".format(
+                    *Product.MIN_RESOLUTION, *Product.MAX_RESOLUTION
+                )
+            )
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_width, min_heigth = Product.MIN_RESOLUTION
+        max_width, max_heigth = Product.MAX_RESOLUTION
+        if image.size > Product.MAX_IMAGE_SIZE:
+            raise ValidationError('Размер файла не должен превышать 3МБ')
+        if img.size[0] < min_width or img.size[1] < min_heigth:
+            raise ValidationError('Разрешение изображения меньше минимального!')
+        if img.size[0] > max_width or img.size[1] > max_heigth:
+            raise ValidationError('Разрешение изображения больше максимального!')
+        return image
+
+
+class ProductAdmin(admin.ModelAdmin):
+    change_form_template = 'custom_admin/change_form.html'
+    #exclude = ('features',)
+
+
+admin.site.register(Category)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Product, ProductAdmin)
---
+++ specs/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import *
+
+
+admin.site.register(ProductFeature)
+admin.site.register(CategoryFeature)
+admin.site.register(FeatureValidator)
